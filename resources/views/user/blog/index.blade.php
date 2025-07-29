d6d06c12148a9c4d0b1ddd055f70380f66add7c9
Ega <eganugrahaid@gmail.com>
2025-07-29 16:14:25 +0700

    penyesuaian dikit (ketinggalan)

--- resources/views/user/blog/index.blade.php
+++ resources/views/user/blog/index.blade.php
@@ -6,16 +6,18 @@
             <div class="container mx-auto px-4 py-8 md:py-12 text-center">
                 <p class="text-sm text-gray-300 mb-2">Fortu Blog</p>
                 <!-- Ukuran font berbeda untuk mobile (text-2xl) dan desktop (md:text-4xl) -->
-                <h1 class="text-2xl md:text-4xl font-bold max-w-2xl mb-6 mx-auto">{{ $hero ? $hero->title : 'Belum ada title' }}</h1>
+                <h1 class="text-2xl md:text-4xl font-bold max-w-2xl mb-6 mx-auto">
+                    {{ $hero ? $hero->title : 'Belum ada title' }}</h1>
                 <div class="relative rounded-lg overflow-hidden">
-                    <img src="{{ $blog[0] ? asset('storage/' . $blog[0]->image) : 'https://placehold.co/1200x500/C7C7C7/FFFFFF?text=IMG+Post' }}" alt="Featured Blog Post"
-                        class="w-full h-auto">
+                    <img src="{{ $blog[0] ? asset('storage/' . $blog[0]->image) : 'https://placehold.co/1200x500/C7C7C7/FFFFFF?text=IMG+Post' }}"
+                        alt="Featured Blog Post" class="w-full h-auto">
                     <!-- Padding dan posisi disesuaikan untuk mobile dan desktop -->
                     <div class="absolute bottom-0 left-0 w-full p-2 md:p-8 text-left">
 
                         <div class="bg-black bg-opacity-50 backdrop-blur-sm p-3 md:p-4 rounded-lg max-w-full md:max-w-sm">
 
-                            <h2 class="text-sm md:text-xl font-bold mb-1 md:mb-2">{{ Str::limit($blog[0] ? $blog[0]->title : 'Belum ada title', 50) }}</h2>
+                            <h2 class="text-sm md:text-xl font-bold mb-1 md:mb-2">
+                                {{ Str::limit($blog[0] ? $blog[0]->title : 'Belum ada title', 50) }}</h2>
 
                             <a href="/blog/{{ $blog[0]->slug }}"
                                 class="text-gray-300 hover:text-white font-semibold text-xs md:text-sm">Read More
@@ -71,20 +73,6 @@
                                 <div
                                     class="border-t border-gray-200 pt-3 flex justify-between items-center text-xs text-gray-500">
                                     <div class="flex space-x-4">
-                                        <span class="flex items-center"><svg class="w-4 h-4" fill="none"
-                                                stroke="currentColor" viewBox="0 0 24 24"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
-                                                </path>
-                                            </svg>{{ $item->likes }}</span>
-                                        <span class="flex items-center"><svg class="w-4 h-4 mr-1" fill="none"
-                                                stroke="currentColor" viewBox="0 0 24 24"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z">
-                                                </path>
-                                            </svg>{{ $item->comments }}</span>
                                         <span class="flex items-center"><svg class="w-4 h-4 mr-1" fill="none"
                                                 stroke="currentColor" viewBox="0 0 24 24"
                                                 xmlns="http://www.w3.org/2000/svg">
@@ -94,6 +82,22 @@
                                                     d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
                                                 </path>
                                             </svg> {{ $item->views }}</span>
+                                        <span class="flex items-center"><svg class="w-4 h-4 mr-1" fill="none"
+                                                stroke="currentColor" viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z">
+                                                </path>
+                                            </svg>{{ $item->comments }}</span>
+
+                                        <span class="flex items-center"><svg class="w-4 h-4" fill="none"
+                                                stroke="currentColor" viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
+                                                </path>
+                                            </svg>{{ $item->likes }}</span>
+
                                     </div>
                                 </div>
                             </div>
@@ -149,7 +153,31 @@
                                             <a href="/blog/${post.slug}" class="hover:text-gray-600">${post.title.length > 75 ? post.title.substring(0, 75) + '...' : post.title}</a>
                                         </h3>
                                         <div class="border-t border-gray-200 pt-3 flex justify-between items-center text-xs text-gray-500">
-                                            {{-- Anda bisa menambahkan stats di sini jika perlu --}}
+                                            <div class="flex space-x-4">
+                                                   <span class="flex items-center"><svg class="w-4 h-4 mr-1" fill="none"
+                                                stroke="currentColor" viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
+                                                </path>
+                                            </svg> ${post.views}</span>
+                                        <span class="flex items-center"><svg class="w-4 h-4 mr-1" fill="none"
+                                                stroke="currentColor" viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z">
+                                                </path>
+                                            </svg>${post.comments}</span>
+                                       <span class="flex items-center"><svg class="w-4 h-4" fill="none"
+                                                stroke="currentColor" viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
+                                                </path>
+                                            </svg>${post.likes}</span>
+                                    </div>
                                         </div>
                                     </div>
                                 </div>
