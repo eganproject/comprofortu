fcca1e98309312fe86f0d3a1b8443ea74ba13082
Ega <eganugrahaid@gmail.com>
2025-08-01 13:59:04 +0700

    footer url

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -8,10 +8,10 @@
                 {{-- Ganti dengan path logo yang benar via asset() helper --}}
                 <img src="{{ $footer['logoUrl'] }}" alt="Logo" class="h-8 w-auto mb-4">
                 <ul class="space-y-2">
-                    <li><a href="#" class="hover:text-purple-600 font-medium">About Us</a></li>
-                    <li><a href="#" class="hover:text-purple-600 font-medium">Service</a></li>
-                    <li><a href="#" class="hover:text-purple-600 font-medium">Product</a></li>
-                    <li><a href="#" class="hover:text-purple-600 font-medium">Contact</a></li>
+                    <li><a href="/about" class="hover:text-purple-600 font-medium">About Us</a></li>
+                    <li><a href="/service" class="hover:text-purple-600 font-medium">Service</a></li>
+                    <li><a href="/product" class="hover:text-purple-600 font-medium">Product</a></li>
+                    <li><a href="/contact" class="hover:text-purple-600 font-medium">Contact</a></li>
                 </ul>
             </div>
 
